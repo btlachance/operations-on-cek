189732868b903bab72b37414613b8e5898e92140
Brian Lachance <blachance@gmail.com>
2017-03-21 18:35:44 -0400

    To delay better var support, simplify main example

--- cek-metalang-ex.rkt
+++ cek-metalang-ex.rkt
@@ -7,15 +7,19 @@
     #:expression
     (e ::= var v (e e))
     (var ::= x y z) ;; For now, our language only has three variables
-                    ;; Eventually, we can replace this with something
-                    ;; like variable-not-otherwise-mentioned
+                    ;; We can remove this limitation later with
+                    ;; something like variable-not-otherwise-mentioned
     (v ::= (lam var e))
     #:env
     (env ::= default-env)
     #:continuation
     (k ::= default-mt (arg e env) (fn e env))
     #:step
-    [(var env k) --> ((lookup env var) env k)]
-    [((e_1 e_2) env k) --> (e_1 env (:: (arg e_2 env) k))]
-    [(v env_0 (:: (arg e env) k)) --> (e env (:: (fn v env_0) k))]
-    [(v env_0 (:: (fn (lam var e) env) k)) --> (e (extend env var v) k)]))
+    [(var env k) --> ((lookup env var) env k)
+     #:implemented-by var]
+    [((e_1 e_2) env k) --> (e_1 env (:: (arg e_2 env) k))
+     #:implemented-by (e e)]
+    [(v env_0 (:: (arg e env) k)) --> (e env (:: (fn v env_0) k))
+     #:implemented-by v]
+    [(v env_0 (:: (fn (lam var e) env) k)) --> (e (extend env var v) k)
+     #:implemented-by v]))
