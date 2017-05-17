b57867905afa83be21addd8602bb5fddea77b058
Brian Lachance <blachance@gmail.com>
2017-05-17 12:31:37 -0400

    Add template parsing for my examples, including variables

--- cek-metalang/prims-impl.rkt
+++ cek-metalang/prims-impl.rkt
@@ -12,7 +12,24 @@
       [(~var v (pattern-metavar ty))
        (pattern-presult ty (list (binding this-syntax ty)))]
       [_ #f]))
-  (form ty parse-variable-pattern #f #f #f #t))
+  (define (bound-as-variable? id bindings)
+    (memf (lambda (b) (binding-equal? b (binding id ty))) bindings))
+  (define (parse-variable-template stx bindings)
+    (syntax-parse stx
+      [(~and (~var v (pattern-metavar ty))
+             ~!
+             (~fail #:unless (bound-as-variable? #'v bindings)
+                    (format "Expected a bound variable of type '~a'" (syntax-e ty))))
+       #t]
+      ;; For now this simple form for variable templates will do. If
+      ;; all nonterminals have a transformer binding, then we can
+      ;; prevent some likely-buggy forms of overlap by seeing if the
+      ;; identifier has a form in its transformer binding and
+      ;; rejecting if it does
+      [id:identifier
+       #t]
+      [_ #f]))
+  (form ty parse-variable-pattern parse-variable-template #f #f (lambda () #t)))
 (module+ test
   (require rackunit)
   ;; These tests don't really live up to the contract on mk-variable
@@ -25,7 +42,11 @@
   (check-binds? (parse-variable-pattern #'variable_1)
                 (binding #'variable_1 #'variable))
   (check-not-binds? (parse-variable-pattern #'number)
-                    (binding #'number #'variable)))
+                    (binding #'number #'variable))
+
+  (define p-var-template (form-parse-template test-var))
+  (check-true (p-var-template #'variable_3 (list (binding #'variable_3 #'variable))))
+  (check-exn exn:fail:syntax? (thunk (p-var-template #'variable_4 (list (binding #'variable #'variable))))))
 
 (define (mk-number ty)
   (define (parse-number-pattern stx)
@@ -33,14 +54,34 @@
       [(~var n (pattern-metavar ty))
        (pattern-presult ty (list (binding this-syntax ty)))]
       [_ #f]))
-  (form ty parse-number-pattern #f #f #f #t))
+  (define (bound-as-number? id bindings)
+    (memf (lambda (b) (binding-equal? b (binding id ty))) bindings))
+  (define (parse-number-template stx bindings)
+    (syntax-parse stx
+      [(~and (~var n (pattern-metavar ty))
+             ~!
+             (~fail #:unless (bound-as-number? #'n bindings)
+                    (format "Expected a bound variable of type '~a'" (syntax-e ty))))
+       #t]
+      [:number #t]
+      [_ #f]))
+  (form ty parse-number-pattern parse-number-template #f #f (lambda () #f)))
 (module+ test
   (define test-num (mk-number #'number))
   (define parse-number-pattern (form-parse-pattern test-num))
+
   (check-binds? (parse-number-pattern #'number)
                 (binding #'number #'number))
   (check-not-binds? (parse-number-pattern #'variable_1)
-                    (binding #'variable_1 #'number)))
+                    (binding #'variable_1 #'number))
+
+  (define p-num-template (form-parse-template test-num))
+  (check-true (p-num-template #'number_2
+                              (list (binding #'number_2 #'number))))
+  (check-true (p-num-template #'10 (list)))
+  (check-false (p-num-template #'"hello" (list)))
+  (check-false (p-num-template #'variable (list)))
+  (check-exn exn:fail:syntax? (thunk (p-num-template #'number (list)))))
 
 (define (mk-numbers ty number-ty)
   (define (parse-numbers-pattern stx)
@@ -58,11 +99,32 @@
           (define result (parse-number-pattern pattern))
           (append (pattern-presult-bindings result) bindings)))]
       [_ #f]))
-  (form ty parse-numbers-pattern #f #f #f #t))
+  (define (parse-numbers-template stx bindings)
+    (syntax-parse stx
+      [(t0 t ...)
+       #:when (free-identifier=? ty #'t0)
+       (define number-f (syntax-local-value number-ty))
+       (define parse-number-template (form-parse-template number-f))
+       (for/and ([template (attribute t)])
+         (parse-number-template template bindings))]
+      ;; TODO run the cons example without this default case. You get
+      ;; an error message that I was not expecting...
+      [_ #f]))
+  (form ty parse-numbers-pattern parse-numbers-template
+        #f #f (lambda () #f)))
 
 (define (parse-alts-pattern alts pattern)
   (for/or ([f (map syntax-local-value alts)])
     ((form-parse-pattern f) pattern)))
+
+(define (parse-alts-template alts template bindings)
+  (define forms* (map syntax-local-value alts))
+  (define (default-form? f) ((form-is-default? f))) ;; yes, ((parens))
+  (define-values (forms default-forms)
+    (partition (compose not default-form?) forms*))
+
+  (for/or ([f (in-sequences forms default-forms)])
+    ((form-parse-template f) template bindings)))
 
 (define (mk-cons ty alts)
   (define alts* (cons ty alts))
@@ -79,4 +141,11 @@
         (append (pattern-presult-bindings t1-presult)
                 (pattern-presult-bindings t2-presult)))]
       [_ #f]))
-  (form ty parse-cons-pattern #f #f #f #f))
+  (define (parse-cons-template stx bindings)
+    (syntax-parse stx
+      [(cons t1 t2)
+       #:with #t (parse-alts-template alts* #'t1 bindings)
+       #:with #t (parse-alts-template alts* #'t2 bindings)
+       #t]
+      [_ #f]))
+  (form ty parse-cons-pattern parse-cons-template #f #f (lambda () #f)))
