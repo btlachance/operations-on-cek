7faf33355a69cc344b4266c2eac4e6f93829c325
Brian Lachance <blachance@gmail.com>
2017-08-25 14:34:03 -0400

    Adapt the pycket_json module so that we can run on cpython again

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,12 +1,23 @@
 import parser
 from runtime import run
-from pycket import pycket_json
-from rpython.rlib import streamio as sio
 
 def main(argv):
+  from rpython.rlib import streamio as sio
+  from pycket import pycket_json
+
   stdin = sio.fdopen_as_stream(0, "r")
-  ast = parser.parse(get_json_string(stdin.readall()))
+  json = pycket_json.loads(stdin.readall())
+  ast = parser.parse(json)
   return run(ast)
 
-def get_json_string(s):
-  return pycket_json.loads(s)
+def main_untranslated(argv):
+  import json
+  import pycket_json_adapter as pja
+
+  adapted = pja.adapt(json.load(sys.stdin))
+  ast = parser.parse(adapted)
+  return run(ast)
+
+if __name__ == "__main__":
+  import sys
+  main_untranslated(sys.argv)
---
+++ interpreter/pycket_json_adapter.py
@@ -0,0 +1,44 @@
+class Adapter(object):
+  def __init__(self, value):
+    self.is_array = self.is_object = self.is_string = self.is_int = False
+
+    if isinstance(value, list):
+      self.is_array = True
+    elif isinstance(value, dict):
+      self.is_object = True
+    elif isinstance(value, unicode):
+      self.is_string = True
+    elif isinstance(value, int) or isinstance(value, long):
+      self.is_int = True
+    else:
+      raise TypeError("Unrecognized value: %s" % value)
+    self.value = value
+
+  def value_array(self):
+    if self.is_array:
+      return [adapt(x) for x in self.value]
+    raise TypeError
+
+  def value_object(self):
+    if self.is_object:
+      return {k: adapt(v) for k, v in self.value.items()}
+    raise TypeError
+
+  def value_string(self):
+    if self.is_string:
+      return self.value
+    raise TypeError
+
+  def value_int(self):
+    if self.is_int:
+      return self.value
+    raise TypeError
+
+  def tostring(self):
+    return json.dumps(self.value)
+
+# Given a value produced by Python's json module, returns a version
+# that is compatible with some of the interface in Pycket's JsonBase
+# from the pycket_json module.
+def adapt(json):
+  return Adapter(json)
